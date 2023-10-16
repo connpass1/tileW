e3437c9d3bcc57e7521e1de9ce4fae0cb4a9bca2
connpass1 <connpass@gmail.com>
2023-10-16 16:57:08 +0300

    jjj

--- app/items/[col]/[uid]/images.tsx
+++ app/items/[col]/[uid]/images.tsx
@@ -12,7 +12,7 @@ export default async function Images({  col ,uid   }:{ col: string ,uid: string}
   if(!Array.isArray(data)) if (!data) return <p>ups</p>; 
     return  <div className="grid grid-cols-2 md:grid-cols-3 gap-4 col-span-2">{ 
         (data as string[]).map((src) =><div key={src}   ><figure className="max-w-lg" >
-        <Image className="h-auto max-w-full rounded-lg " width={200} height={200} src={src} alt="image description"/>
+        <Image className="h-auto max-w-full rounded-lg " width={200} height={200} src={src} priority={false} placeholder ="blur" alt="image description"/>
         <figcaption className="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">Image caption</figcaption>
       </figure> </div>                  
        )}
