4f282fbee1e7717fae54f0115c0ab86013fd6879
connpass1 <connpass@gmail.com>
2023-10-22 18:38:36 +0300

    ggg

--- app/content/page.tsx
+++ app/content/page.tsx
@@ -1,6 +1,18 @@
-import { notFound } from "next/navigation";
+import Link from "next/link";
 
 export default function IndexPageContent(){ 
-     notFound()
+      
+
+     
+         return <div className="col-start-1 col-end-[-1]
+       row-start-1 row-end-[-1] min-h-screen
+        bg-amber-900  dark:bg-zinc-900">
+      
+           <h1>IndexPage</h1> 
+           <div className="flex gap flex-col">
+           <Link href="/content/articles">articles</Link>
+           <Link href="/content/items">items</Link>
+           <Link href="/login">login</Link></div>
+     </div> 
  
 }  
